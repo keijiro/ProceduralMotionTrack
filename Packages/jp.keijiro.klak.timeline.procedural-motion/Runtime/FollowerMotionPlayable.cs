b82b9e5a2900d38f30e83986323e9bc2834fcad7
Keijiro Takahashi <keijiro@gmail.com>
2024-09-15 15:06:42 +0900

    Better blending in FollowerMotion

--- Packages/jp.keijiro.klak.timeline.procedural-motion/Runtime/FollowerMotionPlayable.cs
+++ Packages/jp.keijiro.klak.timeline.procedural-motion/Runtime/FollowerMotionPlayable.cs
@@ -24,92 +24,109 @@ public sealed class FollowerMotionPlayable : PlayableBehaviour
 
     #region Private members
 
-    float3 _p, _vp;
-    float4 _r, _vr;
+    (float3 p, quaternion r) _origin;
+    (float3 p, float4 r) _current;
+    (float3 p, float4 r) _velocity;
     float _prevTime;
 
-    float4 Q2F4(quaternion q) => q.value;
+    static float4 Q2F4(quaternion q) => q.value;
+
+    float CalculateDeltaTime(Playable playable)
+    {
+        var time = (float)playable.GetTime();
+        var dt = math.abs(time - _prevTime);
+        _prevTime = time;
+        return dt;
+    }
 
     #endregion
 
     #region PlayableBehaviour overrides
 
+    // Clip initialization
     public override void OnBehaviourPlay(Playable playable, FrameData info)
-      => _prevTime = -1;
+    {
+        _current = (Target.position, Q2F4(Target.rotation));
+        _velocity = (0, 0);
+        _prevTime = (float)playable.GetTime();
+    }
 
+    // Per-frame initialization
+    public override void PrepareFrame(Playable playable, FrameData info)
+    {
+        var self = info.output.GetUserData() as Transform;
+        if (self == null || Target == null) return;
+
+        // Original transform
+        _origin = (self.position, Q2F4(self.rotation));
+    }
+
+    // Per-frame process
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
         var self = playerData as Transform;
         if (self == null || Target == null) return;
 
-        var time = (float)playable.GetTime();
-
-        if (_prevTime < 0)
-        {
-            (_p, _vp) = (self.position, 0);
-            (_r, _vr) = (Q2F4(self.rotation), 0);
-            _prevTime = time;
-        }
-
-        var dt = math.abs(time - _prevTime);
+        var dt = CalculateDeltaTime(playable);
         var w = info.weight;
 
         if (positionSpeed > 0)
         {
-            var pt = (float3)Target.position;
-            var sp = positionSpeed;
+            var target = (float3)Target.position;
+            var speed = positionSpeed;
 
             if (interpolator == Interpolator.Exponential)
             {
-                _p = math.lerp(pt, _p, math.exp(sp * -dt));
+                _current.p = math.lerp(target, _current.p, math.exp(speed * -dt));
             }
             else if (interpolator == Interpolator.Spring)
             {
-                _vp *= math.exp((1 + sp * 0.5f) * -dt);
-                _vp += (pt - _p) * (sp * 0.1f);
-                _p += _vp * dt;
+                _velocity.p *= math.exp((1 + speed * 0.5f) * -dt);
+                _velocity.p += (target - _current.p) * (speed * 0.1f);
+                _current.p += _velocity.p * dt;
             }
             else // interpolator == Interpolator.DampedSpring
             {
-                var n1 = _vp - (_p - pt) * (sp * sp * dt);
-                var n2 = 1 + sp * dt;
-                _vp = n1 / (n2 * n2);
-                _p += _vp * dt;
+                var n1 = _velocity.p - (_current.p - target) * (speed * speed * dt);
+                var n2 = 1 + speed * dt;
+                _velocity.p = n1 / (n2 * n2);
+                _current.p += _velocity.p * dt;
             }
 
-            self.position = math.lerp(self.position, _p, w);
+            self.position += (Vector3)((_current.p - _origin.p) * w);
         }
 
         if (rotationSpeed > 0)
         {
-            var rt = Q2F4(Target.rotation);
-            var sp = rotationSpeed;
+            var target = Q2F4(Target.rotation);
+            var speed = rotationSpeed;
 
-            if (math.dot(_r, rt) < 0) rt = -rt;
+            if (math.dot(_current.r, target) < 0) target = -target;
 
             if (interpolator == Interpolator.Exponential)
             {
-                _r = math.lerp(rt, _r, math.exp(sp * -dt));
+                _current.r = math.lerp(target, _current.r, math.exp(speed * -dt));
             }
             else if (interpolator == Interpolator.Spring)
             {
-                _vr *= math.exp((1 + sp * 0.5f) * -dt);
-                _vr += (rt - _r) * (sp * 0.1f);
-                _r += _vr * dt;
+                _velocity.r *= math.exp((1 + speed * 0.5f) * -dt);
+                _velocity.r += (target - _current.r) * (speed * 0.1f);
+                _current.r += _velocity.r * dt;
             }
             else // interpolator == Interpolator.DampedSpring
             {
-                var n1 = _vr - (_r - rt) * (sp * sp * dt);
-                var n2 = 1 + sp * dt;
-                _vr = n1 / (n2 * n2);
-                _r += _vr * dt;
+                var n1 = _velocity.r - (_current.r - target) * (speed * speed * dt);
+                var n2 = 1 + speed * dt;
+                _velocity.r = n1 / (n2 * n2);
+                _current.r += _velocity.r * dt;
             }
 
-            var r = math.lerp(Q2F4(self.rotation), _r, w);
-            self.rotation = math.normalize(math.quaternion(r));
+            // self += (current - origin) * w;
+            var r = math.normalize(math.quaternion(_current.r));
+            r = math.mul(math.inverse(_origin.r), r);
+            r = math.nlerp(quaternion.identity, r, w);
+            self.rotation = math.mul(r, self.rotation);
         }
-
-        _prevTime = time;
     }
 
     #endregion
